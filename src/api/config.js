d267329552b0cfb57d6e017ee1e6448c714d3c2c
BeastNeedsMoreTorque <theironhammer1964@gmail.com>
2023-10-05 13:44:10 -0700

    added react-router and cors proxy to vite.config

---
+++ src/api/config.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const apiKey = import.meta.env.VITE_FOOTBALL_API_KEY;
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-Auth-Token': apiKey,
+    }
+}
+
+const BASE_URL = 'https://api.football-data.org/v4/';
+
+export const getMatches (season,league) => {
+    return axios.get(
+        `${BASE_URL}competitions/${season}/matches?=season=${league}`, 
+        options
+    );
+}
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      'api': 'https://api.football-data.org/v4/'
+    }
+  },
   plugins: [react()],
 })
